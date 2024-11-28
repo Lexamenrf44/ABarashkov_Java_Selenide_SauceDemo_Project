67b66ddb5b94b0fe4fbf2ebf2c3d1f7d11d5e90d
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-11-28 11:52:23 +0100

    Minor formatting fix;

--- src/test/java/com/saucedemo/pages/LoginPage.java
+++ src/test/java/com/saucedemo/pages/LoginPage.java
@@ -10,27 +10,19 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
-public class LoginPage {
+public class LoginPage extends BasePage<LoginPage> {
 
-    // Тесты должны быть потокобезопасными (узнать больше), потому что запускаются в многопотоке
-
-    /* LOCATORS */
-
-    // Common
     private final SelenideElement
             title = $(withText("Swag Labs")),
-            errorLabel = $("h3[data-test='error']");
-
-    // Buttons
-    private final SelenideElement
-            loginButton = $("[data-test='login-button']");
-
-    // Fields
-    private final SelenideElement
+            errorLabel = $("h3[data-test='error']"),
+            loginButton = $("[data-test='login-button']"),
             loginField = $("[data-test='username']"),
             passwordField = $("[data-test='password']");
 
-    /* METHODS */
+    @Override
+    public InventoryPage checkThatPageLoaded() {
+        return null;
+    }
 
     public LoginPage doLogin(Username username, Password password) {
         loginField.setValue(username.name());
