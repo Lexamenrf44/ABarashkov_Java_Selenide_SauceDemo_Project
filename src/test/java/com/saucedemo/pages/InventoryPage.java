842860baaf021940d0bc3ea757c2672888304a72
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-11-28 11:52:47 +0100

    Minor formatting fix;

--- src/test/java/com/saucedemo/pages/InventoryPage.java
+++ src/test/java/com/saucedemo/pages/InventoryPage.java
@@ -4,29 +4,19 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 
-public class InventoryPage {
+public class InventoryPage extends BasePage<InventoryPage> {
 
-    /* LOCATORS */
-
-    // Common
     private final SelenideElement
             inventoryContainer = $("[data-test='inventory-container']");
 
-
-    /* METHODS */
-
-    public InventoryPage waitUntilInventoryContainerVisible() {
-        inventoryContainer
-                .shouldBe(visible);
-
-        return this;
+    @Override
+    public InventoryPage checkThatPageLoaded() {
+        return null;
     }
 
-    public InventoryPage manuallyClearCookies() {
-        clearBrowserCookies();
-
+    public InventoryPage waitUntilInventoryContainerVisible() {
+        inventoryContainer.shouldBe(visible);
         return this;
     }
 }
