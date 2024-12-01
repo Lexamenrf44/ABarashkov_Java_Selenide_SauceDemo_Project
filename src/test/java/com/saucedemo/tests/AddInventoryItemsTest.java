076bf02df3c392946e935a36b76b1bd5650de3ce
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-01 19:50:40 +0100

    AddInventoryItemsTest WIP;

--- src/test/java/com/saucedemo/tests/AddInventoryItemsTest.java
+++ src/test/java/com/saucedemo/tests/AddInventoryItemsTest.java
@@ -1,20 +1,21 @@
 package com.saucedemo.tests;
 
+import com.codeborne.selenide.Selenide;
 import com.saucedemo.jupiter.AddCartItem;
 import com.saucedemo.jupiter.CookieLogin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.saucedemo.data.InventoryItems.backpack;
-import static com.saucedemo.data.InventoryItems.tShirt;
+import static com.saucedemo.data.InventoryItems.*;
 
 public class AddInventoryItemsTest extends TestBase {
 
     @CookieLogin
-    @AddCartItem(inventoryitems = {backpack, tShirt})
+    @AddCartItem(inventoryitems = {backpack, tShirt, fleeceJacket, oneSie, bikeLight, jumperRed})
     @Test
     @DisplayName("Should authorize with valid credentials")
     void checkAddedItemsInCart() throws InterruptedException {
+        inventoryPage.navigateToShoppingCart();
 
     }
 }
