74e6967392115a51111d14172771e4a6592dad87
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-01 19:50:03 +0100

    Tests refactoring + fixes;

--- src/test/java/com/saucedemo/tests/CookiesAuthTest.java
+++ src/test/java/com/saucedemo/tests/CookiesAuthTest.java
@@ -2,22 +2,23 @@ package com.saucedemo.tests;
 
 import com.saucedemo.data.LogoutAlerts;
 import com.saucedemo.jupiter.CookieLogin;
-import com.saucedemo.pages.BasePage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.refresh;
-import static com.codeborne.selenide.Selenide.sleep;
-
 public class CookiesAuthTest extends TestBase {
+
+    @CookieLogin
+    @Test
+    @DisplayName("Should authorize with cookies")
+    void shouldAuthorizeWithCookies() {
+        inventoryPage.checkThatPageLoaded();
+    }
 
     @CookieLogin(cookieLifeTimeInSeconds = 3)
     @Test
     @DisplayName("Should logout after cookies spoiled in 3 seconds on [Refresh]")
     void loggedOutAfterCookiesSpoiled() {
-        inventoryPage.waitUntilInventoryContainerVisible();
-        sleep(3500);
-        refresh();
+        inventoryPage.logoutOnRefreshAfterCookiesSpoiled();
         loginPage.loggedOutUserAssertion(LogoutAlerts.INVENTORY_PAGE_ALERT);
     }
 
@@ -25,8 +26,7 @@ public class CookiesAuthTest extends TestBase {
     @Test
     @DisplayName("Should logout after manually clearing cookies on [Refresh]")
     void loggedOutAfterClearingCookiesManually() {
-        inventoryPage.waitUntilInventoryContainerVisible();
-        refresh();
+        inventoryPage.logoutOnRefreshAfterManuallyClearingCookies();
         loginPage.loggedOutUserAssertion(LogoutAlerts.INVENTORY_PAGE_ALERT);
     }
 }
