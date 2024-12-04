f8c88a7ceb779291b09f7cbb8ae5a3472272002c
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-04 15:37:00 +0100

    Filter test implementation;

--- src/test/java/com/saucedemo/web/pages/InventoryPage.java
+++ src/test/java/com/saucedemo/web/pages/InventoryPage.java
@@ -1,20 +1,35 @@
 package com.saucedemo.web.pages;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.saucedemo.data.FilterOptions;
+import com.saucedemo.model.CartModel;
+import com.saucedemo.utils.PriceComparator;
+import com.saucedemo.web.components.FilterComponent;
 import com.saucedemo.web.components.NavbarComponent;
 import io.qameta.allure.Step;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static com.saucedemo.utils.MathUtils.convertToBigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InventoryPage extends BasePage<InventoryPage> {
 
     private final NavbarComponent navbarComponent = new NavbarComponent();
+    private final FilterComponent filterComponent = new FilterComponent();
 
     private final SelenideElement
             inventoryContainer = $("[data-test='inventory-container']"),
-            shoppingCartButton = $("[data-test='shopping-cart-link']");
+            shoppingCartButton = $("[data-test='shopping-cart-link']"),
+            itemName = $("[data-test='inventory-item-name']"),
+            itemDescription = $("[data-test='inventory-item-desc']"),
+            itemPrice = $("[data-test='inventory-item-price']");
+
+    private final ElementsCollection inventoryItems = $$("[data-test='inventory-item']");
 
     @Override
     public InventoryPage checkThatPageLoaded() {
@@ -46,6 +61,56 @@ public class InventoryPage extends BasePage<InventoryPage> {
         refresh();
 
         return new LoginPage().checkThatPageLoaded();
+    }
+
+    @Step("Filter by Price (High to Low)")
+    public InventoryPage filterByPriceHighToLow() {
+        filterComponent.filterByOption(FilterOptions.priceHighToLow);
+
+        List<CartModel> filteredInventoryItems = new ArrayList<>();
+
+        inventoryItems.forEach(e -> {
+            filteredInventoryItems.add(
+                    CartModel.builder()
+                            .title(itemName.getText())
+                            .description(itemDescription.getText())
+                            .price(convertToBigDecimal(itemPrice.getText()))
+                            .build()
+            );
+        });
+
+        List<CartModel> sortedItems = new ArrayList<>(filteredInventoryItems);
+
+        sortedItems.sort(new PriceComparator().reversed());
+
+        assertThat(filteredInventoryItems).isEqualTo(sortedItems);
+
+        return this;
+    }
+
+    @Step("Filter by Price (Low to High)")
+    public InventoryPage filterByPriceLowToHigh() {
+        filterComponent.filterByOption(FilterOptions.priceLowToHigh);
+
+        List<CartModel> filteredInventoryItems = new ArrayList<>();
+
+        inventoryItems.forEach(e -> {
+            filteredInventoryItems.add(
+                    CartModel.builder()
+                            .title(itemName.getText())
+                            .description(itemDescription.getText())
+                            .price(convertToBigDecimal(itemPrice.getText()))
+                            .build()
+            );
+        });
+
+        List<CartModel> sortedItems = new ArrayList<>(filteredInventoryItems);
+
+        sortedItems.sort(new PriceComparator());
+
+        assertThat(filteredInventoryItems).isEqualTo(sortedItems);
+
+        return this;
     }
 
     @Step("Navigage to shopping cart")
