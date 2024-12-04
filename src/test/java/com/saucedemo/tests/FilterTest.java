9ce0b9a252c60b4bd681662b9b2425eca69c67b9
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-04 17:52:41 +0100

    Test implementation with title comparator;

--- src/test/java/com/saucedemo/tests/FilterTest.java
+++ src/test/java/com/saucedemo/tests/FilterTest.java
@@ -4,19 +4,43 @@ import com.saucedemo.jupiter.CookieLogin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.saucedemo.data.FilterOptions.*;
+
 public class FilterTest extends TestBase {
 
     @CookieLogin
     @Test
     @DisplayName("Should filter price high to low")
-    public void filterPriceHighToLowTest() {
-        inventoryPage.filterByPriceHighToLow();
+    public void filterByPriceHighToLowTest() {
+        inventoryPage
+                .filterByOption(priceHighToLow)
+                .assertByOption(priceHighToLow);
     }
 
     @CookieLogin
     @Test
     @DisplayName("Should filter price low to high")
-    public void filterPriceLowToHighTest() {
-        inventoryPage.filterByPriceLowToHigh();
+    public void filterByPriceLowToHighTest() {
+        inventoryPage
+                .filterByOption(priceLowToHigh)
+                .assertByOption(priceLowToHigh);
+    }
+
+    @CookieLogin
+    @Test
+    @DisplayName("Should filter by name A to Z")
+    public void filterByNameAToZTest() {
+        inventoryPage
+                .filterByOption(nameAtoZ)
+                .assertByOption(nameAtoZ);
+    }
+
+    @CookieLogin
+    @Test
+    @DisplayName("Should filter by name by Z to A")
+    public void filterByNameZToATest() {
+        inventoryPage
+                .filterByOption(nameZtoA)
+                .assertByOption(nameZtoA);
     }
 }
