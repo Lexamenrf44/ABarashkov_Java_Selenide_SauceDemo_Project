42d38e68c8c824ef4691f028e05277794fe89c7e
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-04 17:50:13 +0100

    Constants implementation for CartModel;

--- src/test/java/com/saucedemo/model/CartModel.java
+++ src/test/java/com/saucedemo/model/CartModel.java
@@ -1,11 +1,14 @@
 package com.saucedemo.model;
 
+import com.codeborne.selenide.SelenideElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
+
+import static com.saucedemo.utils.MathUtils.convertToBigDecimal;
 
 @Builder
 @Data
@@ -15,4 +18,11 @@ public class CartModel {
     private String title;
     private String description;
     private BigDecimal price;
+
+    public CartModel(SelenideElement element) {
+
+        this.title = element.$("[data-test='inventory-item-name']").getText();
+        this.description = element.$("[data-test='inventory-item-desc']").getText();
+        this.price = convertToBigDecimal(element.$("[data-test='inventory-item-price']").getText());
+    }
 }
