67ed4ae4fb4a13cc6989352047e208027214cea9
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-12-04 17:50:59 +0100

    Lombok builder implementation for FilterOptions

--- src/test/java/com/saucedemo/data/FilterOptions.java
+++ src/test/java/com/saucedemo/data/FilterOptions.java
@@ -1,18 +1,21 @@
 package com.saucedemo.data;
 
+import com.saucedemo.model.CartModel;
+import com.saucedemo.utils.PriceComparator;
+import com.saucedemo.utils.TitleComparator;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Comparator;
+
+@RequiredArgsConstructor
+@Getter
 public enum FilterOptions {
-    nameAtoZ("Name (A to Z)"),
-    nameZtoA("Name (Z to A)"),
-    priceLowToHigh("Price (low to high)"),
-    priceHighToLow("Price (high to low)");
+    nameAtoZ("Name (A to Z)", new TitleComparator()),
+    nameZtoA("Name (Z to A)", new TitleComparator().reversed()),
+    priceLowToHigh("Price (low to high)", new PriceComparator()),
+    priceHighToLow("Price (high to low)", new PriceComparator().reversed());
 
     private final String filterOption;
-
-    FilterOptions(String filterOption) {
-        this.filterOption = filterOption;
-    }
-
-    public String getFilterOption() {
-        return filterOption;
-    }
+    private final Comparator<CartModel> comparator;
 }
